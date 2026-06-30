e4c381138af415e25f3b5443b05e2217c26793fa
Paul Lefizelier <paul@jobpass.com>
2026-06-30 12:16:22 +0200

    fix

--- app/app.vue
+++ app/app.vue
@@ -1,27 +1,23 @@
 <script setup>
 useHead({
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
+  meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+  link: [{ rel: "icon", href: "/favicon.ico" }],
   htmlAttrs: {
-    lang: 'en'
-  }
-})
+    lang: "en",
+  },
+});
 
-const title = 'Nuxt Starter Template'
-const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+const title = "Prism Surf Advisor";
+const description = "Conseiller IA • choix de planche";
 
 useSeoMeta({
   title,
   description,
   ogTitle: title,
   ogDescription: description,
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterCard: 'summary_large_image'
-})
+  ogImage: "/logo-prism-surfboards.png.webp",
+  twitterCard: "summary_large_image",
+});
 </script>
 
 <template>
